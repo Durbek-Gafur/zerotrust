3b62233f84e9a2599c7a2977d73cffb3059fe9cd
Durbek-Gafur <durbek.gafurov@gmail.com>
2023-04-05 05:18:29 +0000

    iperf with policies

--- attack_data/jitter/experiment.sh
+++ attack_data/jitter/experiment.sh
@@ -6,14 +6,14 @@ if [ $# -ne 1 ]; then
 fi
 
 PREFIX="$1"
-RECEIVER_IP=$(kubectl get pod ground -o jsonpath='{.status.podIP}')
-IFACE="eth0"  # Replace this with the appropriate network interface on the ground pod if necessary
+RECEIVER_IP=$(kubectl get pod drone1 -o jsonpath='{.status.podIP}')
+IFACE="eth0"  # Replace this with the appropriate network interface on the drone1 pod if necessary
 DURATION=50  # Duration in seconds for each test
 VIDEO_PORT=4444  # Port used by ffmpeg
 HPING_PORT=5555  # Port used by hping3
 
-# Remove any existing .pcap files from the ground pod before starting the experiment
-kubectl exec  ground --  rm -f *.pcap
+# Remove any existing .pcap files from the drone1 pod before starting the experiment
+kubectl exec  drone1 --  rm -f *.pcap
 
 declare -a HPING_OPTIONS=("fast" "faster" "flood")
 
@@ -21,13 +21,13 @@ for opt in "${HPING_OPTIONS[@]}"; do
     for i in {1..3}; do
         echo "Running experiment with hping3 $opt, iteration $i"
 
-        # Start the video streaming on drone1
-        kubectl exec  drone1 -- sh -c 'ffmpeg -i input.mp4 -f mpegts udp://'"$RECEIVER_IP"':'"$VIDEO_PORT"' >/dev/null 2>&1 &' &
+        # Start the video streaming on ground
+        kubectl exec  ground -- sh -c 'ffmpeg -i input.mp4 -f mpegts udp://'"$RECEIVER_IP"':'"$VIDEO_PORT"' >/dev/null 2>&1 &' &
         sleep 2
 
-        # Start tcpdump on ground and save the output to a file
+        # Start tcpdump on drone1 and save the output to a file
         OUTPUT_FILE="${PREFIX}_tcpdump_output_${opt}_${i}.pcap"
-        kubectl exec  ground -- timeout $DURATION tcpdump -s 0 -i $IFACE "udp port $VIDEO_PORT" -w $OUTPUT_FILE &
+        kubectl exec  drone1 -- timeout $DURATION tcpdump -s 0 -i $IFACE "udp port $VIDEO_PORT" -w $OUTPUT_FILE &
         pid_tcpdump=$!
         sleep 10
         
@@ -38,21 +38,21 @@ for opt in "${HPING_OPTIONS[@]}"; do
         # Wait for the specified duration before stopping the tcpdump process
         sleep $DURATION
 
-        # Stop tcpdump on ground
-        kubectl exec  ground -- pkill tcpdump
+        # Stop tcpdump on drone1
+        kubectl exec  drone1 -- pkill tcpdump
         wait $pid_tcpdump
 
-        # Stop the video streaming on drone1
-        kubectl exec  drone1 -- pkill ffmpeg
+        # Stop the video streaming on ground
+        kubectl exec  ground -- pkill ffmpeg
 
         # Stop hping3 on drone3
         kubectl exec  drone3 -- pkill hping3
 
         # Download the output file to the local machine
-        kubectl cp ground:$OUTPUT_FILE "${PREFIX}_tcpdump_output_${opt}_${i}.pcap"
+        kubectl cp drone1:$OUTPUT_FILE "${PREFIX}_tcpdump_output_${opt}_${i}.pcap"
 
-        # Remove the output file from the ground pod
-        kubectl exec  ground -- rm -f $OUTPUT_FILE
+        # Remove the output file from the drone1 pod
+        kubectl exec  drone1 -- rm -f $OUTPUT_FILE
 
         # Sleep for 50 seconds before starting the next iteration
         sleep 50
